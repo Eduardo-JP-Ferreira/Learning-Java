bed692bd3ac14844041291e5bfc282b835337077
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2024-01-21 20:37:09 -0300

    Add Exercise-1

--- Ex1/src/App.java
+++ Ex1/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+
+    Scanner sc = new Scanner(System.in);
+    String signal = "Negativo";
+    String type = "Impar";
+    System.out.println("Digite um numero:");
+    int num = sc.nextInt();
+
+    if (num > 0) {
+      signal = "Positivo";
+    }
+    if (num % 2 == 0) {
+      type = "Par";
+    }
+    System.out.println(num + " é " + type + " e " + signal);
   }
 }
